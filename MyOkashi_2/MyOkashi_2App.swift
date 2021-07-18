e8e64fa5f74688d22391670da9d99a35490798aa
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-07-18 10:02:59 +0900

    Initial Commit

---
+++ MyOkashi_2/MyOkashi_2App.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashi_2App.swift
+//  MyOkashi_2
+//
+//  Created by administrator on 2021/07/18.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashi_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
