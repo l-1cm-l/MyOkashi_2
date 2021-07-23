8f7174a103ba5b22615dbb1c5905dad55c9b8378
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-07-23 09:56:52 +0900

    [processing]

--- MyOkashi_2/OkashiData.swift
+++ MyOkashi_2/OkashiData.swift
@@ -10,5 +10,16 @@ import Foundation
 class OkashiData: ObservableObject{
     func serchOkashi(keyword: String){
         print(keyword)
+        
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else{
+            return
+        }
+        
+        
+        guard let req_url = URL(string: "https://sysbird/toriko/api/?apikey=guest?format=json&keyword=\(keyword_encode)%max=10&order=r")
+        else{
+            return
+        }
+        print(req_url)
     }
 }
